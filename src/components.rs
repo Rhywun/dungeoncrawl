2b533e5ec19af1e7208cd5764e7fdef8d6edb7b2
Rhywun <patrick.huffer@proton.me>
2024-04-18 23:54:42 -0400

    End of Ch. 7 - adding a message

--- src/components.rs
+++ src/components.rs
@@ -12,5 +12,13 @@ pub struct Player;
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Enemy;
 
+/// The [Entity] this is attached to moves randomly.
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct MovingRandomly;
+
+/// A message that indicates the [Entity] wants to move.
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct WantsToMove {
+    pub entity: Entity,
+    pub destination: Point,
+}
--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -1,24 +1,19 @@
 use crate::prelude::*;
 
 #[system]
-#[write_component(Point)]
+#[read_component(Point)]
 #[read_component(Player)]
 pub fn player_input(
     ecs: &mut SubWorld,
-    #[resource] map: &Map,
+    commands: &mut CommandBuffer,
     #[resource] key: &Option<VirtualKeyCode>,
-    #[resource] camera: &mut Camera,
     #[resource] turn_state: &mut TurnState,
 ) {
+    let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
+
     if let Some(key) = key {
         // Debug
         eprintln!("{:?}", key);
-
-        // // NOTE: Debug - commit suicide
-        // if *key == VirtualKeyCode::X {
-        //     // TODO: Quit
-        //     return;
-        // }
 
         // Commit suicide
         if *key == VirtualKeyCode::X {
@@ -28,10 +23,18 @@ pub fn player_input(
         }
 
         let delta = match key {
-            VirtualKeyCode::Left | VirtualKeyCode::A | VirtualKeyCode::Numpad4 => Point::new(-1, 0),
-            VirtualKeyCode::Right | VirtualKeyCode::D | VirtualKeyCode::Numpad6 => Point::new(1, 0),
-            VirtualKeyCode::Up | VirtualKeyCode::W | VirtualKeyCode::Numpad8 => Point::new(0, -1),
-            VirtualKeyCode::Down | VirtualKeyCode::S | VirtualKeyCode::Numpad2 => Point::new(0, 1),
+            VirtualKeyCode::Left
+            | VirtualKeyCode::A
+            | VirtualKeyCode::Numpad4 => Point::new(-1, 0),
+            VirtualKeyCode::Right
+            | VirtualKeyCode::D
+            | VirtualKeyCode::Numpad6 => Point::new(1, 0),
+            VirtualKeyCode::Up
+            | VirtualKeyCode::W
+            | VirtualKeyCode::Numpad8 => Point::new(0, -1),
+            VirtualKeyCode::Down
+            | VirtualKeyCode::S
+            | VirtualKeyCode::Numpad2 => Point::new(0, 1),
             VirtualKeyCode::Numpad7 => Point::new(-1, -1),
             VirtualKeyCode::Numpad9 => Point::new(1, -1),
             VirtualKeyCode::Numpad1 => Point::new(-1, 1),
@@ -39,19 +42,33 @@ pub fn player_input(
             _ => Point::zero(),
         };
 
-        if delta.x != 0 || delta.y != 0 {
-            // This means something like "get all the points that have the
-            // player."
-            let mut players = <&mut Point>::query().filter(component::<Player>());
+        // Add a message that the player wants to move.
+        players.iter(ecs).for_each(|(entity, pos)| {
+            let destination = *pos + delta;
+            commands.push((
+                (),
+                WantsToMove {
+                    entity: *entity,
+                    destination,
+                },
+            ));
+        });
+        *turn_state = TurnState::PlayerTurn;
 
-            players.iter_mut(ecs).for_each(|pos| {
-                let destination = *pos + delta;
-                if map.can_enter_tile(destination) {
-                    *pos = destination;
-                    camera.on_player_move(destination);
-                    *turn_state = TurnState::PlayerTurn;
-                }
-            });
-        }
+        // if delta.x != 0 || delta.y != 0 {
+        //     // This means something like "get all the points that have the
+        //     // player."
+        //     let mut players =
+        //         <&mut Point>::query().filter(component::<Player>());
+
+        //     players.iter_mut(ecs).for_each(|pos| {
+        //         let destination = *pos + delta;
+        //         if map.can_enter_tile(destination) {
+        //             *pos = destination;
+        //             camera.on_player_move(destination);
+        //             *turn_state = TurnState::PlayerTurn;
+        //         }
+        //     });
+        // }
     }
 }
